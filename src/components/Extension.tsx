5d385a0215c3e0ddd97bc205d8d0f097b33140dc
Soham Pagi <paguisoham36@gamail.com>
2025-06-02 02:12:47 +0530

    Fix image location

--- src/components/Extension.tsx
+++ src/components/Extension.tsx
@@ -10,7 +10,7 @@ export default function Extension(props: TypeExtensionProps) {
     return (
         <div className={'flex flex-col gap-6 bg-white border border-black/30 rounded-3xl p-6 md:p-4 dark:bg-[hsl(226,25%,17%)] dark:border-white/30'}>
             <div className={'flex gap-5'}>
-                <img width={80} height={80} className={'size-16'} src={props.logo} alt="Extension Logo" />
+                <img width={80} height={80} className={'size-16'} src={`/browser-extension-manager${props.logo}`} alt="Extension Logo" />
                 <span class={'space-y-1'}>
                     <h4 className={'font-bold text-2xl md:text-xl dark:text-white/95'}>{props.name}</h4>
                     <p class={'text-xl text-black/50 md:text-md dark:text-white/60'}>{props.description}</p>
